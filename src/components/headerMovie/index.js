d0aee379e431a65d32bc61bcd5d2e2c48d74c22c
Bryan Keane <20089166@mail.wit.ie>
2021-11-05 15:55:50 +0000

    Fix backward/forward arrows.

--- src/components/headerMovie/index.js
+++ src/components/headerMovie/index.js
@@ -6,43 +6,43 @@ import Paper from "@material-ui/core/Paper";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import HomeIcon from "@material-ui/icons/Home";
+import { withRouter } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    justifyContent: "space-around",
-    flexWrap: "wrap",
-    padding: theme.spacing(1.5),
-    margin: 0,
-  },
-  tagLine: {
-    fontSize: "1.5rem",
-  },
+    root: {
+        display: "flex",
+        justifyContent: "space-around",
+        flexWrap: "wrap",
+        padding: theme.spacing(1.5),
+        margin: 0,
+    },
+    tagLine: {
+        fontSize: "1.5rem",
+    },
 }));
 
-const MovieHeader = (props) => {
-  const classes = useStyles();
-  const movie = props.movie;
+const MovieHeader = ({movie, history}) => {
+    const classes = useStyles();
 
-  return (
-    <Paper component="div" className={classes.root}>
-      <IconButton aria-label="go back">
-        <ArrowBackIcon color="primary" fontSize="large" />
-      </IconButton>
+    return (
+        <Paper component="div" className={classes.root}>
+            <IconButton aria-label="go back" onClick={() => history.goBack()} >
+                <ArrowBackIcon color="primary" fontSize="large" />
+            </IconButton>
 
-      <Typography variant="h4" component="h3">
-        {movie.title}
-        <a href={movie.homepage}>
-          <HomeIcon color="primary" />
-        </a>
-        <br />
-        <span className={classes.tagLine}>{`   "${movie.tagline}"`} </span>
-      </Typography>
-      <IconButton aria-label="go forward">
-        <ArrowForwardIcon color="primary" fontSize="large" />
-      </IconButton>
-    </Paper>
-  );
+            <Typography variant="h4" component="h3">
+                {movie.title}
+                <a href={movie.homepage}>
+                    <HomeIcon color="primary" />
+                </a>
+                <br />
+                <span className={classes.tagLine}>{`   "${movie.tagline}"`} </span>
+            </Typography>
+            <IconButton aria-label="go forward" onClick={() => history.goForward() } >
+                <ArrowForwardIcon color="primary" fontSize="large" />
+            </IconButton>
+        </Paper>
+    );
 };
 
-export default MovieHeader;
+export default withRouter(MovieHeader);
--- src/components/headerMovieList/index.js
+++ src/components/headerMovieList/index.js
@@ -1,41 +1,37 @@
 import React from "react";
 import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
-import { makeStyles } from "@material-ui/core/styles";
+import {makeStyles} from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
+import { withRouter } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    justifyContent: "space-around",
-    flexWrap: "wrap",
-    marginBottom: theme.spacing(1.5),
-  },
+    root: {
+        display: "flex",
+        justifyContent: "space-around",
+        flexWrap: "wrap",
+        marginBottom: theme.spacing(1.5),
+    },
 }));
 
-const Header = (props ) => {
-  const classes = useStyles();
-  const title = props.title
-  return (
-    <Paper component="div" className={classes.root}>
-      <IconButton
-        aria-label="go back"
-      >
-        <ArrowBackIcon color="primary" fontSize="large" />
-      </IconButton>
+const Header = ({title, history}) => {
+    const classes = useStyles();
+    return (
+        <Paper component="div" className={classes.root}>
+            <IconButton aria-label="go back" onClick={() => history.goBack()}>
+                <ArrowBackIcon color="primary" fontSize="large"/>
+            </IconButton>
 
-      <Typography variant="h4" component="h3">
-        {title}
-      </Typography>
-      <IconButton
-        aria-label="go forward"
-      >
-        <ArrowForwardIcon color="primary" fontSize="large" />
-      </IconButton>
-    </Paper>
-  );
+            <Typography variant="h4" component="h3">
+                {title}
+            </Typography>
+            <IconButton aria-label="go forward" onClick={() => history.goForward()}>
+                <ArrowForwardIcon color="primary" fontSize="large" />
+            </IconButton>
+        </Paper>
+    );
 };
 
-export default Header;
+export default withRouter(Header);
